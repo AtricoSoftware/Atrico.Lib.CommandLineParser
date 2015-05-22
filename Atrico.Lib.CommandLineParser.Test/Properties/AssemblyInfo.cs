1c6c6867c9a493c618a9289d72183edc944641ff
Rob Rimmer <developer@atrico.net>
2015-05-22 09:23:35 +0100

    Initial project creation

---
+++ Atrico.Lib.CommandLineParser.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Atrico.Lib.CommandLineParser.Test")]
+[assembly: AssemblyProduct("Atrico.Lib.CommandLineParser.Test")]
+[assembly: AssemblyDescription("Command Line Parser unit tests")]
---
+++ Atrico.Lib.CommandLineParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Atrico.Lib.CommandLineParser")]
+[assembly: AssemblyProduct("Atrico.Lib.CommandLineParser")]
+[assembly: AssemblyDescription("Command Line Parser")]
