2ec1642cd0170899f5e67341e956bf3679bd5757
Rob Rimmer <developer@atrico.net>
2015-05-27 16:03:18 +0100

    Missing files added

---
+++ Atrico.Lib.CommandLineParser/Exceptions/Options/NoSetterException.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Atrico.Lib.CommandLineParser.Exceptions.Options
+{
+    /// <summary>
+    ///     A property has no setter
+    /// </summary>
+    public class NoSetterException : InvalidOptionsPropertyException
+    {
+        public NoSetterException(PropertyInfo property)
+            : base(property, string.Format("Property has no setter: {0}", property.Name))
+        {
+        }
+    }
+}
---
+++ Atrico.Lib.CommandLineParser/Exceptions/Options/UnSupportedTypeException.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Atrico.Lib.CommandLineParser.Exceptions.Options
+{
+    /// <summary>
+    ///     A property is an unsupported type
+    /// </summary>
+    public class UnSupportedTypeException : InvalidOptionsPropertyException
+    {
+        public UnSupportedTypeException(PropertyInfo property)
+            : base(property, string.Format("Type not supported: {0}", property.PropertyType.Name))
+        {
+        }
+    }
+}
